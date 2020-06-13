f44166377db86d629d7a4f65dd624d998c75dfd9
sanjay sharma <sanjay@MacBook-Pro.local>
2020-06-14 02:31:23 +1000

    Multiple series comparison

--- app/src/app/dashboard/dashboard.component.ts
+++ app/src/app/dashboard/dashboard.component.ts
@@ -5,16 +5,6 @@ import { interval } from 'rxjs';
 import { StockPriceHistory } from '../common/stock-price-history';
 import * as Highcharts from 'highcharts/highstock';
 
-declare var require: any;
-let Boost = require('highcharts/modules/boost');
-let noData = require('highcharts/modules/no-data-to-display');
-let More = require('highcharts/highcharts-more');
-
-Boost(Highcharts);
-noData(Highcharts);
-More(Highcharts);
-noData(Highcharts);
-
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
@@ -24,10 +14,10 @@ export class DashboardComponent implements OnInit {
 
   constructor(private stockPriceService: StockPriceService) { }
 
-  ausymbols = ['IIND.AX', 'NDIA.AX', 'OOO.AX', 'GEAR.AX', 'NAB.AX','TLS.AX','AUDINR=X'];
-  uksymbols = ['RB.L', 'BATS.L', 'RTO.L','CCL.L', 'TSCO.L', 'RR.L'];
-  currencies = ['AUD','USD','INR'];
-  currencyPairs = [];
+  symbols: string[] = ['IIND.AX','NDIA.AX','^NSEI'];
+  currencies = ['AUD', 'USD', 'INR'];
+  currencyPairs: string[] = [];
+  compPairs = {};
 
   stockPrices = {};
   stockPriceHistories = {};
@@ -42,20 +32,20 @@ export class DashboardComponent implements OnInit {
   }
 
   buildCurrencyPairs() {
-    for(var i = 0; i < this.currencies.length; i++)
-    {
-      for(var j = 0; j < this.currencies.length; j++)
-      {
-        if(i !== j)
-        {
+    for (var i = 0; i < this.currencies.length; i++) {
+      for (var j = 0; j < this.currencies.length; j++) {
+        if (i !== j) {
+          let cPair = this.currencies[i] + this.currencies[j] + "=X";
+          this.compPairs[cPair] = [cPair];
           this.currencyPairs.push(this.currencies[i] + this.currencies[j] + "=X");
         }
       }
     }
+    this.symbols = this.symbols.concat(this.currencyPairs);
   }
 
   update() {
-    this.ausymbols.forEach(element => {
+    this.symbols.forEach(element => {
       this.stockPriceService
         .getStockPrice(element)
         .subscribe(data => {
@@ -64,129 +54,234 @@ export class DashboardComponent implements OnInit {
         })
     });
 
-    
-    this.currencyPairs.forEach(element => {     
+
+    this.symbols.forEach(element => {
       this.stockPriceService
         .getStockPriceHistory(element)
         .subscribe(data => {
-          this.stockPriceHistories[element] = data;
-          var TwoDArrayData = this.prepareChartData(data);
+          let ohlcData = this.prepareChartData(data);
+          let lineData = this.prepareChartData(data, 'line');
           this.chart[element] = {
-            title: element,
-            type: 'Line',
-            width: '800px',
-            height: 800,
-            data: TwoDArrayData     
+            ohlc: ohlcData,
+            line: lineData
           };
         })
-      });    
+    });
   }
 
-  onSelectStock(symbol)  {   
+  onSelectStock(symbol) {
     this.stockPriceService
       .getStockPriceHistory(symbol)
       .subscribe(data => {
         this.stockPriceHistories[symbol] = data;
+        this.compPairs[symbol] = [symbol];
         this.setChart(data, symbol);
+        this.renderSingleSeriesChart(symbol);
         this.selectedSymbol = symbol;
       })
   }
 
-  renderChart(symbol, data) {
+  onCompare(bSymbol, cSymbol) {
+    this.compPairs[bSymbol].push(cSymbol);
+    this.renderCompareSeriesChart(this.compPairs[bSymbol]);
+  }
+
+  renderSingleSeriesChart(symbol) {
     Highcharts.stockChart('chart-' + symbol, {
 
-
       rangeSelector: {
-          selected: 1
+        selected: 1
       },
 
       title: {
-          text: symbol
+        text: symbol
       },
 
       series: [{
-          name: symbol,
-          data: data,
-          tooltip: {
-              valueDecimals: 2
-          },
-          type: 'ohlc'
+        name: symbol,
+        data: this.chart[symbol]['ohlc'],
+        tooltip: {
+          valueDecimals: 2
+        },
+        type: 'ohlc'
       }]
-  });
+    });
   }
 
   private setChart(data: StockPriceHistory[], symbol: any) {
-    var TwoDArrayData = this.prepareChartData(data);
+    let ohlcData = this.prepareChartData(data);
+    let lineData = this.prepareChartData(data, 'line');
+
     this.chart[symbol] = {
-      title: symbol,
-      type: 'Line',
-      width: '800px',
-      height: 800,
-      data: TwoDArrayData
+      'ohlc': ohlcData,
+      'line': lineData 
     };
-    
-    this.renderChart(symbol, TwoDArrayData)
   }
 
-  onCurrencyChange(symbol, currency){
-    var stockPrice = this.stockPrices[symbol];
+  renderCompareSeriesChart(symbols: string[]) {
+    let seriesOptions: any[] = symbols.map(x => { 
+      let lineData = this.chart[x]['line'];
+      return {        
+        name: x, data: lineData 
+      }
+    });
 
-    if(stockPrice.currency === currency)
-    {
+    Highcharts.stockChart('chart-' + symbols[0], {
+
+      rangeSelector: {
+        selected: 4
+      },
+
+      yAxis: {
+        labels: {
+          formatter: function () {
+            return (this.value > 0 ? ' + ' : '') + this.value + '%';
+          }
+        },
+        plotLines: [{
+          value: 0,
+          width: 2,
+          color: 'silver'
+        }]
+      },
+
+      plotOptions: {
+        series: {
+          compare: 'percent',
+          showInNavigator: true
+        }
+      },
+
+      tooltip: {
+        pointFormat: '<span style="color:{series.color}">{series.name}</span>: <b>{point.y}</b> ({point.change}%)<br/>',
+        valueDecimals: 2,
+        split: true
+      },
+
+      series: seriesOptions
+    });
+  }
+
+
+
+  onCurrencyChange(symbol, currency) {
+    var stockPrice = this.stockPrices[symbol];
+    this.compPairs[symbol] = [symbol];
+
+    if (stockPrice.currency === currency) {
       this.setChart(this.stockPriceHistories[symbol], symbol);
       this.stockPrices[symbol].viewCurrency = currency;
-      return;     
-    }
-
-    let currencyPair = stockPrice.viewCurrency + currency + "=X";
-    let adjustedSph = this.adjustForCurrency(
-      this.stockPriceHistories[symbol], 
-      this.stockPriceHistories[currencyPair]);
-    
-    if(adjustedSph.length === 0)
-    {
+      this.renderSingleSeriesChart(symbol);
       return;
     }
 
-    this.chart[symbol].data = adjustedSph;
+    let currencyPair = stockPrice.viewCurrency + currency + "=X";
+    let adjustedSphOhlc = this.adjustForCurrencyOhlc(
+      this.chart[symbol]['ohlc'],
+      this.chart[currencyPair]['ohlc']);
+    
+    if (adjustedSphOhlc.length === 0) {
+      return;
+    }
+
+    this.chart[symbol]['ohlc'] = adjustedSphOhlc;
+
+    this.renderSingleSeriesChart(symbol);
+
+    let adjustedSphLine = this.adjustForCurrencyOhlc(
+      this.chart[symbol]['line'],
+      this.chart[currencyPair]['line']);
+    
+    if (adjustedSphLine.length === 0) {
+      return;
+    }
+
+    this.chart[symbol]['line'] = adjustedSphLine;
+
     this.stockPrices[symbol].viewCurrency = currency;
   }
 
-  prepareChartData(data: StockPriceHistory[]) {
+  prepareChartData(data: StockPriceHistory[], type: string = 'ohlc') {
     let finalData = [];
     let cnt = data.length - 1;
-    data.forEach(e => {
-      let ohlc = [e.date * 1000, e.open, e.high, e.low, e.close];
-      cnt--;
-      finalData.push(ohlc);
-    });
+    switch (type) {
+      case 'ohlc':
+        data.forEach(e => {
+          let ohlc = [e.date * 1000, e.open, e.high, e.low, e.close];
+          cnt--;
+          finalData.push(ohlc);
+        });
+        break;
+      case 'line':
+        data.forEach(e => {
+          let line = [e.date * 1000, e.close];
+          cnt--;
+          finalData.push(line);
+        });
+    }
+
 
     return finalData;
   }
 
-  adjustForCurrency(existing: StockPriceHistory[], currency: StockPriceHistory[]) {
-    let min1 = Math.min.apply(Math, existing.map(o => o.date));
-    let min2 = Math.min.apply(Math, currency.map(o => o.date));
-    let max1 = Math.max.apply(Math, existing.map(o => o.date));
-    let max2 = Math.max.apply(Math, currency.map(o => o.date));
+  adjustForCurrencyOhlc(existing: any[], currency: any[]) {
+    let min1 = Math.min.apply(Math, existing.map(o => o[0]));
+    let min2 = Math.min.apply(Math, currency.map(o => o[0]));
+    let max1 = Math.max.apply(Math, existing.map(o => o[0]));
+    let max2 = Math.max.apply(Math, currency.map(o => o[0]));
 
     let min = Math.max(min1, min2);
     let max = Math.min(max1, max2);
+    let fHis: any[] = existing
+      .filter(x => x[0] >= min && x[0] <= max)
+      .sort(x => x[0]);
 
-    let newStockPriceHistory: StockPriceHistory[] = existing
-      .filter(x => x.date >= min && x.date <= max)
-      .sort(x => x.date);
-
-    let newCurrency: StockPriceHistory[] = currency
-      .filter(x => x.date >= min && x.date <= max)
-      .sort(x => x.date);
+    let fCur: any[] = currency
+      .filter(x => x[0] >= min && x[0] <= max)
+      .sort(x => x[0]);
 
     let finalData = [];
 
-    for(var i=0; i < Math.min(newStockPriceHistory.length, newCurrency.length); i++) {
-      let ohlc = [new Date(newStockPriceHistory[i].date * 1000), 
-        newStockPriceHistory[i].close * newCurrency[i].close];
-        finalData.push(ohlc);        
+    for (var i = 0; i < Math.min(fHis.length, fCur.length); i++) {
+      let ohlc = [
+        fHis[i][0],
+        fHis[i][1] * fCur[i][1],
+        fHis[i][2] * fCur[i][2],
+        fHis[i][3] * fCur[i][3],
+        fHis[i][4] * fCur[i][3]
+      ];
+
+      finalData.push(ohlc);
+    }
+
+    return finalData;
+  }
+
+  adjustForCurrencyLine(existing: any[], currency: any[]) {
+    let min1 = Math.min.apply(Math, existing.map(o => o[0]));
+    let min2 = Math.min.apply(Math, currency.map(o => o[0]));
+    let max1 = Math.max.apply(Math, existing.map(o => o[0]));
+    let max2 = Math.max.apply(Math, currency.map(o => o[0]));
+
+    let min = Math.max(min1, min2);
+    let max = Math.min(max1, max2);
+    let fHis: any[] = existing
+      .filter(x => x[0] >= min && x[0] <= max)
+      .sort(x => x[0]);
+
+    let fCur: any[] = currency
+      .filter(x => x[0] >= min && x[0] <= max)
+      .sort(x => x[0]);
+
+    let finalData = [];
+
+    for (var i = 0; i < Math.min(fHis.length, fCur.length); i++) {
+      let ohlc = [
+        fHis[i][0],
+        fHis[i][1] * fCur[i][1]
+      ];
+
+      finalData.push(ohlc);
     }
 
     return finalData;
@@ -216,7 +311,7 @@ export class DashboardComponent implements OnInit {
       if (newStockPrice.bid > this.stockPrices[symbol].bid) {
         newStockPrice.tickChangePositive = true;
       }
-      
+
       else if (newStockPrice.bid < this.stockPrices[symbol].bid) {
         newStockPrice.tickChangePositive = false;
       }
